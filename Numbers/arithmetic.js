8df2cd272096c3770372281f7f7b5bf5c9cdf6a4
shanky251020 <shashank16septd@gmail.com>
2023-09-20 17:35:37 +0530

    Added arithmetic operation in javascript

---
+++ Numbers/arithmetic.js
@@ -0,0 +1,44 @@
+// javascript arithmetic operators
+
+// Types of arithmetic operators
+// +(add) , -(sub) , *(mul) , /(quotient) , %(rem) , **(expone) , ++(increment) , --(decrement)
+
+// addition
+let x = 10 +5;
+console.log(x);   // 15
+
+// subtraction
+let a =9;
+let b = 5;
+console.log(a-b);  
+
+// multiplication
+let c = 25;
+let d = 5;
+console.log(c*d); // 125
+
+// / division
+
+let w = 100;
+let z =10;
+console.log(100/10); // 10 (quotient)
+
+// % operator 
+
+let g = 100;
+let h =10;
+console.log(g%h); // 0  (remainder)
+
+// ++ operator 
+
+let e = 100;
+e++;
+let r = e;
+console.log(r);
+
+// simillary -- operator works
+
+// ** exponentiation 
+
+let t = 5**2;
+console.log(t); 
