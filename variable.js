3d18dcd7338152745b477411d9b2c877ed0110a0
Shashank <shashank16septd@gmail.com>
2024-05-02 12:51:44 +0530

    Added variable.js

---
+++ variable.js
@@ -0,0 +1,20 @@
+let message = "hello world";   //variable declaration and definition
+console.log(message);          // o/p ; Hello world
+message = "hello universe";    // value in variable message is changed to hello universe and previous value deleted
+console.log(message);          // o/p : Hello universe
+
+// constants 
+
+const myBirthday = "1.10.1999";
+console.log(myBirthday);    // 1.10.1999
+// myBirthday = '1.11.1999';  // cannot change the const value
+// console.log(myBirthday);
+
+
+//Exercises :--
+
+let admin;
+let adname = "John";
+admin = adname;
+alert(admin);
+
