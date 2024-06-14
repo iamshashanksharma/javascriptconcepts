f9e1a4feebb59d124bde251c3b5e07f806662401
Shashank <shashank16septd@gmail.com>
2024-06-14 17:43:41 +0530

    Added conditional branching

---
+++ conditionalbranching.js
@@ -0,0 +1,40 @@
+/* if clause
+if(year == 2015)
+{
+    . .........
+}
+
+else clause 
+
+if(year == 2015 )
+{
+    ........
+}
+else{
+    ........
+}
+
+conditional operator
+let result = condition ? value1 : value2
+
+*/
+
+/*Exercises */
+
+//1 rewrite\
+/* 
+let result;
+let a =1;
+let b = 2;
+if(a + b < 4)
+{
+    console.log("below")
+}
+else{
+    console.log("over");
+}
+
+*/
+let a =1;
+let b =2;
+let result = a+b < 4 ? console.log("below") : console.log("above");
