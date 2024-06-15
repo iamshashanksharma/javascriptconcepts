0b6b74956571d6ab49fefcb7984cb57fc61d405f
Shashank <shashank16septd@gmail.com>
2024-06-15 18:36:07 +0530

    Added arrays declaration

---
+++ arrays.js
@@ -0,0 +1,6 @@
+// syntax for arrays is let arr = [];
+
+let fruits = ["apple", 'mango', 'pear'];
+console.log(fruits[0]);   // Apple
+console.log(fruits[1]);   // Mango
+console.log(fruits[2]);   // Pear
